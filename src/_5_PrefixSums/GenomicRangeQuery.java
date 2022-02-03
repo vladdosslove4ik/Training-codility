6d3c8d4a948c314dcde9ef78c0ca52dd05673009
Vladyslav Nahurnik <vladnagura@gmail.com>
2022-02-03 04:17:55 +0100

    Lesson 5 GenomicRangeQuery 62% points solution

--- src/_5_PrefixSums/GenomicRangeQuery.java
+++ src/_5_PrefixSums/GenomicRangeQuery.java
@@ -5,49 +5,24 @@ import java.util.Arrays;
 public class GenomicRangeQuery {
 
     public static void main(String[] args) {
-        int[] P = {2, 5, 6, 0};
-        int[] Q = {4, 5, 6, 8};
+        int[] P = {2, 5, 0};
+        int[] Q = {4, 5, 6};
         System.out.println(Arrays.toString(solution("CAGCCTA", P, Q)));
     }
 
     public static int[] solution(String S, int[] P, int[] Q) {
-        System.out.println('A' < 'T');
 
         int n = P.length;
         int[] answers = new int[n];
 
         for (int i = 0; i < n; i++) {
+            String subS = S.substring(P[i], Q[i]+1);
 
-            int start = P[i];
-            int end = Q[i];
-
-            if (start == end) {
-                answers[i] = impactFactor(S.charAt(start));
-            } else {
-                for (int j = start; j < end; j++) {
-                    int impactFactor = impactFactor(S.charAt(j));
-                    if (impactFactor == 1) {
-                        answers[i] = impactFactor;
-                        break;
-                    } else if (impactFactor < answers[i] || answers[i] == 0)
-                        answers[i] = impactFactor;
-                }
-            }
+            if(subS.contains("A"))  answers[i] = 1;
+            else if(subS.contains("C")) answers[i] = 2;
+            else if(subS.contains("G")) answers[i] = 3;
+            else answers[i] = 4;
         }
         return answers;
-    }
-
-    public static int impactFactor(char l) {
-        switch (l) {
-            case ('A'):
-                return 1;
-            case ('C'):
-                return 2;
-            case ('G'):
-                return 3;
-            case ('T'):
-                return 4;
-        }
-        return -1;
     }
 }
